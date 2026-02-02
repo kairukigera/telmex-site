eb743174de694d2f9283cdd3cb2e2a1844fe9f74
kairukigera <kairukigera@gmail.com>
2026-02-02 12:01:27 +0300

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,13 +1,10 @@
-const mysql = require('mysql2');
-require('dotenv').config();
+const { Pool } = require('pg');
 
-const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-module.exports = db;
+module.exports = pool;
